b535361a646b9e671a9b2570e98a5e7fe98f9d87
NielsVLD <nielskrommenhoek@hotmail.nl>
2024-03-10 22:37:20 +0100

    Add loading spinner to about page

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Portfolio
 
 My portfolio website made with Angular and hosted on GitHub Pages.
+
+Go to https://nielsvld.github.io/Portfolio/ to visit!
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, MatProgressSpinner, NgIf],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
   profilePicture: string =
     'https://onedrive.live.com/embed?resid=6BFB86DAE0ADC616%213480&authkey=%21ALranRe6JLeHfAQ&width=660';
+
+  loaded: boolean = false;
+
+  onImageLoad() {
+    this.loaded = true;
+  }
 }
