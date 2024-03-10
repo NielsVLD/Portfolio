12de79b3ef5f01ff3c6470b233890e9ef78a84f8
NielsVLD <nielskrommenhoek@hotmail.nl>
2024-03-10 19:45:39 +0100

    Add prettier formatter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import {HeaderComponent} from "./components/header/header.component";
 
-export const routes: Routes = [
-  { path: 'header', component: HeaderComponent },
-];
+export const routes: Routes = [];
