5a4c4b0012d4cc00b59243f4effbfb4d650b55ab
NielsVLD <nielskrommenhoek@hotmail.nl>
2024-03-15 19:37:48 +0100

    Add home page component - add some images

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  OnChanges,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { NgIf, NgOptimizedImage } from '@angular/common';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
